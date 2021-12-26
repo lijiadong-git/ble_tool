e16efd25d7f08e2d4f34a00f6a7981eec45d587f
lijiadong <664320100@qq.com>
2021-12-26 19:13:47 +0800

    DebugActivity

--- app/src/main/java/com/zxw/ui/DebugActivity.java
+++ app/src/main/java/com/zxw/ui/DebugActivity.java
@@ -2,9 +2,7 @@ package com.zxw.ui;
 
 import android.os.Bundle;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.TextView;
-
 
 import com.zxw.R;
 
@@ -13,60 +11,55 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class DebugActivity extends BasActivity implements View.OnClickListener{
+public class DebugActivity extends BasActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_debug);
-        initViev();
-        initData();
+        initView();
     }
 
-    private void initData() {
-
-    }
-
-    private void initViev() {
-        super.getContext(this);
-        setButton(R.id.debug_read);
+    private void initView() {
+        View view = findViewById(R.id.debug_read);
+        if (view != null) {
+            view.setOnClickListener(this);
+        }
     }
 
     @Override
     public void onClick(View v) {
         TextView textView = findViewById(R.id.bug_log);
-
-        textView.setText(load("errNewLog"));
+        if (textView != null) {
+            textView.setText(load("errNewLog"));
+        }
     }
 
-
-
-    public String load(String file){
+    public String load(String file) {
         FileInputStream in = null;
         BufferedReader reader = null;
         StringBuilder content = new StringBuilder();
-        try{
+        try {
             in = openFileInput(file);
             reader = new BufferedReader(new InputStreamReader(in));
             String line = "";
-            while ((line = reader.readLine()) != null){
+            while ((line = reader.readLine()) != null) {
                 content.append(line);
             }
-        }catch (IOException e){
+        } catch (IOException e) {
             e.printStackTrace();
-        }finally {
-            if(reader != null){
-                try{
+        } finally {
+            if (reader != null) {
+                try {
                     reader.close();
-                }catch (IOException e){
+                } catch (IOException e) {
                     e.printStackTrace();
                 }
             }
         }
-        if(!content.toString().isEmpty())
-            return  content.toString();
-        else
+        if (content.toString().isEmpty()) {
             return "没有记录到异常";
+        }
+        return content.toString();
     }
-
 }
